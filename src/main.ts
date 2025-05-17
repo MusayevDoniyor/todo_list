71cc98262b70ae14784e389a6924db7ab63ac85e
MusayevDoniyor <doniyormusayev2009@gmail.com>
2025-05-17 15:36:10 +0500

    - configure global pipe, swagger - set global prefix - improve start:dev console message

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Todo List')
+    .setDescription('Todo List Api')
+    .build();
+  const factory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, factory);
+
+  const PORT = process.env.PORT ?? 3000;
+  await app
+    .listen(PORT)
+    .then(() => console.log(`🚀 Server is running on http://localhost:${PORT}`))
+    .catch((err) => console.error(`❌ ${err.message}`));
 }
 bootstrap();
